df4f81c3c4b835a25b6c80f96e3098fbe78eb804
Luis Saucedo <a2019111501@estudiantes.upsa.edu.bo>
2021-01-15 20:24:08 -0400

    UNITtest de DIVISION GET y POST

--- WEB_calculadora/NUnitTestCalculadora/UnitTest1.cs
+++ WEB_calculadora/NUnitTestCalculadora/UnitTest1.cs
@@ -10,18 +10,18 @@ namespace NUnitTestCalculadora
         {
         }
         //-----------------------------------------------------------
-        // UNITtest MULTIPLICACION POST
+        // UNITtest DIVISION POST
         // POST 1
         [Test]
-        public void TestMultiplicacionPOST1()
+        public void TestDivisionPOST1()
         {
             // Arrange = preparacion
-            int num1 = 12;
-            int num2 = 12;
-            int esperado = 144;
+            int num1 = 50;
+            int num2 = 50;
+            int esperado = 1;
             CalculadoraController calc = new CalculadoraController();
             // Act = ejecucion
-            int resultado = calc.multiplicacionpost(num1, num2);
+            float resultado = calc.divisionpost(num1, num2);
             // Assert = verificacion
             Assert.AreEqual(esperado, resultado);
 
@@ -29,15 +29,15 @@ namespace NUnitTestCalculadora
         //-----------------------------------------------------------
         // POST 2
         [Test]
-        public void TestMultiplicacionPOST2()
+        public void TestDivisionPOST2()
         {
             // Arrange = preparacion
-            int num1 = 10;
-            int num2 = 10;
-            int esperado = 100;
+            int num1 = 25;
+            int num2 = 5;
+            int esperado = 5;
             CalculadoraController calc = new CalculadoraController();
             // Act = ejecucion
-            int resultado = calc.multiplicacionpost(num1, num2);
+            float resultado = calc.divisionpost(num1, num2);
             // Assert = verificacion
             Assert.AreEqual(esperado, resultado);
 
@@ -45,32 +45,32 @@ namespace NUnitTestCalculadora
         //-----------------------------------------------------------
         // POST 3
         [Test]
-        public void TestMultiplicacionPOST3()
+        public void TestDivisionPOST3()
         {
             // Arrange = preparacion
-            int num1 = 5;
-            int num2 = 7;
-            int esperado = 35;
+            int num1 = 21;
+            int num2 = 3;
+            int esperado = 7;
             CalculadoraController calc = new CalculadoraController();
             // Act = ejecucion
-            int resultado = calc.multiplicacionpost(num1, num2);
+            float resultado = calc.divisionpost(num1, num2);
             // Assert = verificacion
             Assert.AreEqual(esperado, resultado);
 
         }
         //-----------------------------------------------------------
-        // UNITtest MULTIPLICACION GET
+        // UNITtest DIVISION GET
         // GET 1
         [Test]
-        public void TestMultiplicacionGET1()
+        public void TestDivisionGET1()
         {
             // Arrange = preparacion
-            int num1 = 2;
-            int num2 = 2;
-            int esperado = 4;
+            int num1 = 50;
+            int num2 = 5;
+            int esperado = 10;
             CalculadoraController calc = new CalculadoraController();
             // Act = ejecucion
-            int resultado = calc.multiplicacionget(num1, num2);
+            float resultado = calc.divisionget(num1, num2);
             // Assert = verificacion
             Assert.AreEqual(esperado, resultado);
 
@@ -78,15 +78,15 @@ namespace NUnitTestCalculadora
         //-----------------------------------------------------------
         // GET 2
         [Test]
-        public void TestMultiplicacionGET2()
+        public void TestDivisionGET2()
         {
             // Arrange = preparacion
-            int num1 = 11;
-            int num2 = 11;
-            int esperado = 121;
+            int num1 = 20;
+            int num2 = 10;
+            int esperado = 2;
             CalculadoraController calc = new CalculadoraController();
             // Act = ejecucion
-            int resultado = calc.multiplicacionget(num1, num2);
+            float resultado = calc.divisionget(num1, num2);
             // Assert = verificacion
             Assert.AreEqual(esperado, resultado);
 
@@ -94,15 +94,15 @@ namespace NUnitTestCalculadora
         //-----------------------------------------------------------
         // GET 3
         [Test]
-        public void TestMultiplicacionGET3()
+        public void TestDivisionGET3()
         {
             // Arrange = preparacion
-            int num1 = 8;
-            int num2 = 8;
-            int esperado = 64;
+            int num1 = 49;
+            int num2 = 7;
+            int esperado = 7;
             CalculadoraController calc = new CalculadoraController();
             // Act = ejecucion
-            int resultado = calc.multiplicacionget(num1, num2);
+            float resultado = calc.divisionget(num1, num2);
             // Assert = verificacion
             Assert.AreEqual(esperado, resultado);
 
